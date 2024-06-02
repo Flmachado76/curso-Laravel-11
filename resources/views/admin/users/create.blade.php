aa3da43c3b6c828d19f81badf5e1dfe0e1f9015f
Flmachado76 <flmmac@gmail.com>
2024-06-02 20:45:43 -0300

    atualização aula 10

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,10 +1,26 @@
-<h1>Novo Usuário</h1>
+@extends('admin.layouts.app')
 
-<form action="{{ route('users.store') }}" method="POST">
-    @csrf()
-    <input type="text" name="name" placeholder="Nome">
-    <input type="text" name="email" placeholder="E-mail">
-    <input type="password" name="password" placeholder="Senha">
+@section('title', 'Novo Usuário')
 
-    <button type="submit">Cadastrar</button>
-</form>
+@section('content')
+
+    <h1>Novo Usuário</h1>
+
+    @if($errors->any())
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+    <form action="{{ route('users.store') }}" method="POST">
+        @csrf()
+        <input type="text" name="name" placeholder="Nome" value="{{ old('name') }}">
+        <input type="text" name="email" placeholder="E-mail" value="{{ old('email') }}">
+        <input type="password" name="password" placeholder="Senha">
+
+        <button type="submit">Cadastrar</button>
+    </form>
+
+@endsection
